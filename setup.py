737e5e70da04fd513eddb94ccbada483fdeaa066
Telminov Sergey <telminov@soft-way.biz>
2015-03-01 14:13:59 +0300

    base structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(
+    name='django-park-keeper',
+    version='0.0.1',
+    description='Monitoring and services administration.',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/shuffler',
+    packages=['parkkeeper',],
+    license='The MIT License',
+)
