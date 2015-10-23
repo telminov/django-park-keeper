9dca8dc97eb702d163532f9daeadff8b8bac0b03
sergey@telminov.ru <sergey@telminov.ru>
2015-10-23 10:21:58 +0300

    zmq request-replay sample

--- parkkeeper/keeper.py
+++ parkkeeper/keeper.py
@@ -1,63 +1,93 @@
 # coding: utf-8
-from multiprocessing import Process, Queue
 from time import sleep
 import uuid
 import random
+import zmq
 
-class Keeper:
-    STOP_MSG = 'STOP'
+STOP_MSG = 'STOP'
 
-    def __init__(self, monits_count=2):
-        self.monit_workers_count = monits_count
-        self.monit_workers = []
-        self.monit_queue = Queue()
 
-    def start_loop(self):
-        self._start_monits()
-
-        try:
-            while True:
-                for w in self._get_alive_monit_workers():
-                    task = str(random.randint(1, 100))
-                    self.monit_queue.put(task)
-
-                sleep(1)
-        finally:
-            print('Stop them all!..')
-            self._stop_workers()
-            sleep(1)
-
-    def _start_monits(self):
-        for worker_id in range(0, self.monit_workers_count):
-            monit_worker = MonitWorker()
-            monit_worker.setup(self.monit_queue, worker_id=str(worker_id))
-            monit_worker.start()
-            self.monit_workers.append(monit_worker)
-
-    def _stop_workers(self):
-        for w in self._get_alive_monit_workers():
-            self.monit_queue.put(self.STOP_MSG)
-
-    def _get_alive_monit_workers(self):
-        return [w for w in self.monit_workers if w.is_alive()]
-
-class MonitWorker(Process):
-    worker_id = None
-    task_queue = None
-
-    def setup(self, task_queue: Queue, worker_id: str=None):
-        self.worker_id = worker_id or str(uuid.uuid4())
-        self.task_queue = task_queue
-
-    def run(self):
-        print('working %s started' % self.worker_id)
+class Requester:
+    def start(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://localhost:5559")
+        print('Requester start')
 
         while True:
-            task = self.task_queue.get()
-            sleep(0.5)
+            msg = str(random.randint(1, 100))
+            socket.send_string(msg)
+            replay = socket.recv()
+            print('Replay %s' % replay)
+            sleep(1)
 
-            if task == Keeper.STOP_MSG:
-                print('working %s stopped' % self.worker_id)
-                break
+class Replayer:
+    def __init__(self, worker_id):
+        self.worker_id = worker_id
 
-            print('monit %s working: %s...' % (self.worker_id, task))
+    def start(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5559")
+        print('Replayer start')
+
+        while True:
+            req_msg = socket.recv_string()
+            print("Received request: %s" % req_msg)
+            rep_msg = 'Worker %s process %s' % (self.worker_id, req_msg)
+            socket.send_string(rep_msg)
+
+
+#
+# class Keeper:
+#
+#     def __init__(self, monits_count=2):
+#         self.monit_workers_count = monits_count
+#         self.monit_workers = []
+#
+#     def start_loop(self):
+#         self._start_monits()
+#
+#         try:
+#             while True:
+#                 for w in self._get_alive_monit_workers():
+#                     task = str(random.randint(1, 100))
+#                     self.add_monit_task(task)
+#
+#                 sleep(1)
+#         finally:
+#             print('Stop them all!..')
+#             self._stop_workers()
+#             sleep(1)
+#
+#     def add_monit_task(self, task):
+#         pass
+#
+#     def _stop_workers(self):
+#         for w in self._get_alive_monit_workers():
+#             self.add_monit_task(STOP_MSG)
+#
+#     def _get_alive_monit_workers(self):
+#         return [w for w in self.monit_workers if w.is_alive()]
+#
+#
+# class MonitWorker:
+#     worker_id = None
+#
+#     def __init__(self, worker_id: str=None):
+#         self.worker_id = worker_id or str(uuid.uuid4())
+#         self.context = zmq.Context()
+#         receiver = self.context.socket(zmq.)
+#
+#     def run(self):
+#         print('working %s started' % self.worker_id)
+#
+#         while True:
+#             task = self.task_queue.get()
+#             sleep(0.5)
+#
+#             if task == Keeper.STOP_MSG:
+#                 print('working %s stopped' % self.worker_id)
+#                 break
+#
+#             print('monit %s working: %s...' % (self.worker_id, task))
--- parkkeeper/management/commands/start_monit_workers.py
+++ parkkeeper/management/commands/start_monit_workers.py
@@ -1,16 +1,12 @@
 # coding: utf-8
 from django.core.management import BaseCommand
 # from parkkeeper.keeper import Keeper
-from parkkeeper.keeper import Requester
+from parkkeeper.keeper import Replayer
 
 class Command(BaseCommand):
     help = 'Start main background keeper process for scheduling jobs'
 
     def handle(self, *args, **options):
-        requester = Requester()
-        requester.start()
+        replayer = Replayer(1)
+        replayer.start()
         # keeper = Keeper()
-        # keeper.start_loop()
-
-
-
