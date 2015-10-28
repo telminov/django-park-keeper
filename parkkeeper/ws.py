6c14b01cca3c99fa7f0e264ef76390c9d85ce78a
sergey@telminov.ru <sergey@telminov.ru>
2015-10-28 12:37:36 +0300

    add WebSocketHandler with background process

--- parkkeeper/ws.py
+++ parkkeeper/ws.py
@@ -4,6 +4,7 @@ import asyncio
 import json
 from aiohttp import web, MsgType
 from django.utils.timezone import now
+from parkkeeper import models
 import zmq
 
 def start_server():
@@ -31,52 +32,87 @@ def add_routes(app):
     app.router.add_route('GET', '/monits', MonitResultHandler().get_handler)
 
 
-
 class WebSocketHandler(metaclass=ABCMeta):
     ws = None
+    stop_msg = 'close_ws'
+    need_background = False
+    stop_background_timeout = 1
+
+    async def process_msg(self, msg_text):
+        print(msg_text)
+
+
+    async def background(self):
+        while not self.ws.closed:
+            print(now())
+            await asyncio.sleep(1)
+        print('Close background')
+
 
     async def get_handler(self, request):
         self.ws = web.WebSocketResponse()
         await self.ws.prepare(request)
 
-        while not self.ws.closed:
-            msg = await self.ws.receive()
+        # start background process if needed
+        background_task = None
+        if self.need_background:
+            loop = asyncio.get_event_loop()
+            background_task = loop.create_task(self.background())
 
-            if msg.tp == MsgType.text:
-                await self.process_msg(msg)
-            elif msg.tp == MsgType.close:
-                print('websocket connection closed')
-            elif msg.tp == MsgType.error:
-                print('ws connection closed with exception %s' %
-                      self.ws.exception())
+        # process ws messages
+        while not self.ws.closed:
+            await self._receive_msg()
+
+        # stop background
+        if background_task:
+            background_task.cancel()
 
         return self.ws
 
-    @abstractclassmethod
-    async def process_msg(self, msg):
-        if msg.data == 'close':
-            await self.ws.close()
-        else:
-            self.ws.send_str(msg.data + '/answer')
+    async def _receive_msg(self):
+        msg = await self.ws.receive()
+
+        if msg.tp == MsgType.text:
+            if msg.data == self.stop_msg:
+                print('Got stop msg')
+                await self.ws.close()
+            else:
+                await self.process_msg(msg.data)
+        elif msg.tp == MsgType.close:
+            print('websocket connection closed')
+        elif msg.tp == MsgType.error:
+            print('ws connection closed with exception %s' %
+                  self.ws.exception())
 
 
 class MonitResultHandler(WebSocketHandler):
-    async def process_msg(self, msg):
+    need_background = True
+
+    async def background(self):
         context = zmq.Context()
         subscriber_socket = context.socket(zmq.SUB)
         subscriber_socket.connect("tcp://localhost:5561")
         subscriber_socket.setsockopt(zmq.SUBSCRIBE, b'')
 
         try:
-            while True:
+            while not self.ws.closed:
                 print(now().isoformat())
                 try:
-                    status = subscriber_socket.recv_json(flags=zmq.NOBLOCK)
-                    print(status)
-                    self.ws.send_str(json.dumps(status))
+                    task_json = subscriber_socket.recv_json(flags=zmq.NOBLOCK)
+                    task = models.MonitTask.from_json(task_json)
+                    print(task)
+                    response = {
+                        'monit_name': task.monit_name,
+                        'host_address': task.host_address,
+                        'schedule_id': task.schedule_id,
+                        'result_dt': task.result.dt.isoformat(sep=' '),
+                        'extra': task.result.extra,
+                        'is_success': task.result.is_success,
+                    }
+                    self.ws.send_str(json.dumps(response))
                 except zmq.error.Again:
                     pass
-                await asyncio.sleep(1)
+                await asyncio.sleep(0.5)
         finally:
             subscriber_socket.close()
             context.term()
