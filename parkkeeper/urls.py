d51caa64c2b073eb2d7800633d7fea8d64b4de66
sergey@telminov.ru <sergey@telminov.ru>
2015-12-02 13:08:33 +0300

    update to django 1.9

--- parkkeeper/urls.py
+++ parkkeeper/urls.py
@@ -11,13 +11,13 @@ router.register('monit_schedule', views.MonitScheduleViewSet)
 router.register('work_schedule', views.WorkScheduleViewSet)
 
 
-urlpatterns = patterns('parkkeeper.views',
-    url(r'^$', 'index'),
-    url(r'^monit_status_latest/$', 'monit_status_latest'),
-    url(r'^work_status_latest/$', 'work_status_latest'),
-    url(r'^monit_task/(.+)/$', 'monit_task'),
-    url(r'^work_task/(.+)/$', 'work_task'),
-)
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^monit_status_latest/$', views.monit_status_latest),
+    url(r'^work_status_latest/$', views.work_status_latest),
+    url(r'^monit_task/(.+)/$', views.monit_task),
+    url(r'^work_task/(.+)/$', views.work_task),
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
